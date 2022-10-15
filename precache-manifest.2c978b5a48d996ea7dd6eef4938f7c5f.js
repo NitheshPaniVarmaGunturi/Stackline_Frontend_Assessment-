455c3c2d3febdb666a6f82ca319e0dbbb9d8fccd
nithesh varma <nithesh@dynamic-10-125-175-19.dynamic.unt.edu>
2022-10-14 19:53:48 -0500

    Updates

---
+++ precache-manifest.2c978b5a48d996ea7dd6eef4938f7c5f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd923505feaba22d2972f76e3309dce9",
+    "url": "/Stackline_Frontend_Assessment-/index.html"
+  },
+  {
+    "revision": "bacb3702e7452cefac83",
+    "url": "/Stackline_Frontend_Assessment-/static/css/2.bfa46653.chunk.css"
+  },
+  {
+    "revision": "e999502b59f5a61d0142",
+    "url": "/Stackline_Frontend_Assessment-/static/css/main.5cc43112.chunk.css"
+  },
+  {
+    "revision": "bacb3702e7452cefac83",
+    "url": "/Stackline_Frontend_Assessment-/static/js/2.297086a5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Stackline_Frontend_Assessment-/static/js/2.297086a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e999502b59f5a61d0142",
+    "url": "/Stackline_Frontend_Assessment-/static/js/main.d6c69087.chunk.js"
+  },
+  {
+    "revision": "cb748e33f95b5221e1ee",
+    "url": "/Stackline_Frontend_Assessment-/static/js/runtime-main.a596ee65.js"
+  },
+  {
+    "revision": "64b21497ca2ad205d903035392064f38",
+    "url": "/Stackline_Frontend_Assessment-/static/media/stackline_logo.64b21497.svg"
+  }
+]);
